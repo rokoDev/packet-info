7ee41aea2024ece0232ab278ab8fe021ee7cb78c
rokoDev <mishaDoter@gmail.com>
2021-05-05 17:12:02 +0300

    initial commit

---
+++ tests/src/main_tests.cpp
@@ -0,0 +1,11 @@
+#include <fmt/core.h>
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+
+    const auto testResult = RUN_ALL_TESTS();
+
+    return testResult;
+}
---
+++ tests/src/packet_tests.cpp
@@ -0,0 +1,5 @@
+#include <fmt/core.h>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+TEST(PacketInfo, FirstTest) { ASSERT_EQ(0, 0); }
